0236cd8ecdadef01fb17791aaa6832f83f37be53
stalvin2001 <stalvinmanjali@gmail.com>
2024-04-09 16:40:12 +0530

    logout-credits-component

---
+++ src/app/shared.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router'; 
+import { DataTransferService } from './data-transfer.service';
+import { AuthServiceService } from './auth-service.service';
+import * as firebase from 'firebase/compat';
+import { getAuth, signOut } from "firebase/auth";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedService {
+
+  constructor(private router: Router, private dataTransferService:DataTransferService, private authService : AuthServiceService) { }
+
+  logout(){
+
+    const auth = getAuth();
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      console.log("User Logged Out");
+      this.router.navigate(['/']);
+    }).catch((error) => {
+      // An error happened.
+    });
+  }
+
+
+}
