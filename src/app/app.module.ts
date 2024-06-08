e89aa855e76b3c59e5e2238619cdc21fce2cb72e
sudhirVoid <bhandarisudhir4274@gmail.com>
2024-06-08 23:59:47 +0530

    animation module adde.d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ChapterUiComponent } from './chapter-ui/chapter-ui.component';
@@ -32,6 +33,7 @@ import { CreditsModalComponent } from './credits-modal/credits-modal.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     RouterModule,
     HttpClientModule,
